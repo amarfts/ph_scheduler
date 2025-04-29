894f569cd3467bdfdac759d85e4aa151392ec339
fettis <amar@student.efpl.be>
2025-04-29 23:07:05 +0000

    url externe vm google

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ function LoginPage() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { username, password });
+      const res = await axios.post("http://34.32.89.181/api/auth/login", { username, password });
 
       localStorage.setItem("token", res.data.token);
 
