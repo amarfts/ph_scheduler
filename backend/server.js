c32de66cb0292df86cfa83e6a8fdf2e817f1062b
fettis <amar@student.efpl.be>
2025-04-30 10:12:49 +0000

    SERVEUR ROUTES

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,15 @@ const db = require('./db/database');
 require('dotenv').config();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
+const authRoutes = require('./routes/auth');
+const pharmaciesRoutes = require('./routes/pharmacies');
+const postRoutes = require('./routes/posts');
+
+app.use('/api/auth', authRoutes);      
 app.use('/api/pharmacies', pharmaciesRoutes);
 app.use('/api/posts', postRoutes);
-app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
